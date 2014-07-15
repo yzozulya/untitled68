6de53ef84f49ce9abe67831f64eefb6c30019b48
Yulia.Zozulya <yuzozulya@gmail.com>
2014-07-15 11:33:44 +0400

    test

---
+++ a.py
@@ -0,0 +1,4 @@
+__author__ = 'Yulia.Zozulya'
+
+class a(object):
+    pass
---
+++ c.py
@@ -0,0 +1,8 @@
+print("\nNewline is highlighted.")
+
+import re
+print("\nNewline is NOT highlighted.")
+
+regex = re.compile(r'\bbackslash-b IS highlighted')
+
+print("\nNewline is NOT highlighted.")
